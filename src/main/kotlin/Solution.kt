65ad8212626550c6a4a5a0577c4310a4e48c4771
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2021-08-01 20:05:15 +0200

    Use BFS for labelling islands

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -2,60 +2,51 @@ class Solution {
     fun largestIsland(grid: Array<IntArray>): Int {
         val n = grid.size
         val sizes = mutableMapOf<Int, Int>()
+        val steps = arrayOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
 
-        fun label(i: Int, j: Int): Int =
-            if (i < 0 || i >= n || j < 0 || j >= n) 0 else grid[i][j]
+        fun label(row: Int, col: Int): Int =
+            if (row < 0 || row >= n || col < 0 || col >= n) 0 else grid[row][col]
+
+        fun conquerIsland(label: Int, row: Int, col: Int): Int {
+            val queue = mutableListOf<Pair<Int, Int>>()
+            var size = 1
+            grid[row][col] = label
+            queue.add(Pair(row, col))
+            while (queue.isNotEmpty()) {
+                val (r, c) = queue.removeAt(0)
+                for (step in steps) {
+                    val nextR = r + step.first
+                    val nextC = c + step.second
+                    if (label(nextR, nextC) == 1) {
+                        size++
+                        grid[nextR][nextC] = label
+                        queue.add(Pair(nextR, nextC))
+                    }
+                }
+            }
+            return size
+        }
 
         fun discoverIslands(firstLabel: Int) {
             require(firstLabel > 1)
             var k = firstLabel
-            for (i in 0 until n) {
-                for (j in 0 until n) {
-                    if (grid[i][j] == 1) {
-                        val above = label(i - 1, j)
-                        val left = label(i, j - 1)
-                        if (above == 0 && left == 0) {
-                            grid[i][j] = k
-                            sizes[k] = 1
-                            k++
-                        } else if (above == 0) {
-                            grid[i][j] = left
-                            sizes.merge(left, 1, Int::plus)
-                        } else if (left == 0) {
-                            grid[i][j] = above
-                            sizes.merge(above, 1, Int::plus)
-                        } else if (above == left) {
-                            grid[i][j] = above
-                            sizes.merge(above, 1, Int::plus)
-                        } else {
-                            grid[i][j] = above
-                            sizes[above] = sizes[above]!! + sizes[left]!! + 1
-                            sizes.remove(left)
-                            for (p in i downTo 0) {
-                                var changed = false
-                                for (q in 0 until n) {
-                                    if (grid[p][q] == left) {
-                                        grid[p][q] = above
-                                        changed = true
-                                    }
-                                }
-                                if (!changed) break
-                            }
-                        }
+            for (row in 0 until n) {
+                for (col in 0 until n) {
+                    if (grid[row][col] == 1) {
+                        val size = conquerIsland(k, row, col)
+                        sizes[k] = size
+                        k++
                     }
                 }
             }
         }
 
-        fun largestCombinedIslandSize(): Int {
+        fun largestCombinedIsland(): Int {
             var maxSize = 0
-            for (i in 0 until n) {
-                for (j in 0 until n) {
-                    if (grid[i][j] == 0) {
-                        val neighbors = setOf(
-                            label(i - 1, j), label(i + 1, j),
-                            label(i, j - 1), label(i, j + 1)
-                        )
+            for (row in 0 until n) {
+                for (col in 0 until n) {
+                    if (grid[row][col] == 0) {
+                        val neighbors = steps.map { (dr, dc) -> label(row + dr, col + dc) }.toSet()
                         val combinedSize = neighbors.map { label -> sizes.getOrDefault(label, 0) }.sum()
                         maxSize = maxOf(combinedSize + 1, maxSize)
                     }
@@ -65,7 +56,7 @@ class Solution {
         }
 
         discoverIslands(2)
-        val largestSingleIslandSize = sizes.values.fold(0) { acc, value -> maxOf(acc, value) }
-        return maxOf(largestCombinedIslandSize(), (largestSingleIslandSize + 1).coerceAtMost(n * n))
+        val largestSingleIsland = sizes.values.fold(0) { acc, value -> maxOf(acc, value) }
+        return maxOf(largestCombinedIsland(), (largestSingleIsland + 1).coerceAtMost(n * n))
     }
 }
