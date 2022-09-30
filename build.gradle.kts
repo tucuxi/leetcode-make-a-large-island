c2b78fb11a16da135d27c89c57ced29f569a5e02
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2022-09-30 23:24:08 +0200

    Enable detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("io.gitlab.arturbosch.detekt").version("1.21.0")
 }
 
 group = "me.kds"
