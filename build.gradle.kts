5fff89f0411758ce93d3f1ef3e8096d136c7d491
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2023-08-20 20:42:48 +0200

    Upgrade gradle wrapper to v8.3, tidy up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.9.0"
-    id("io.gitlab.arturbosch.detekt").version("1.21.0")
+    id("io.gitlab.arturbosch.detekt").version("1.23.1")
 }
 
 group = "me.kds"
@@ -18,8 +16,4 @@ dependencies {
 
 tasks.test {
     useJUnit()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "17"
 }
